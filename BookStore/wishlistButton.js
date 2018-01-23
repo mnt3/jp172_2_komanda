6a11ae017528a54ee10848856925228bf295c321
Mantas Pauliukonis <mantas.pauliukonis3@gmail.com>
2018-01-23 09:45:25 +0200

    wishlistas skaiciuoja suma, ps pirmo nereikia skaiciuoti, suma reaguoja nuo mygtuko paspaudimo buy books lange

--- BookStore/wishlistButton.js
+++ BookStore/wishlistButton.js
@@ -1,5 +1,6 @@
 /* wishlist mygtukas*/
-
+var sum = 0;
+var sumVieta = document.getElementById("totalPrice");
 /*paspausto mygtuko funkcija*/
 var masyvas = [knyga1,knyga2,knyga3,knyga4,knyga5,knyga6,knyga7,knyga8,knyga9,knyga10];
 
@@ -8,7 +9,13 @@ function  wishbuttonpush(paspBusena){
     
     
     sukurtiWishlist(masyvas[paspBusena-1]);
+    sumCalculate(masyvas[paspBusena-1]);
     
+}
+
+function sumCalculate(knygas){
+  sum=sum+Number(knygas.price);
+  sumVieta.innerHTML="<p>Eur</p><p>"+sum+"</p>";
 }
 
 
@@ -19,3 +26,9 @@ var paspaustasMygtukas = document.getElementById("buybook1");
 paspaustasMygtukas.onclick= function(){
   sukurtiWishlist(knyga1);  
 }*/
+
+/* bendros sumos apskaiciavimas*/ 
+
+
+sumVieta.innerHTML="<p>Eur</p><p>"+sum+"</p>";
+//sum.innerText="20";
